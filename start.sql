0413f7f489b0b4d65c09648b0f6123fc1229d3a6
Irene Williams <europeandistribution@ymail.com>
2020-08-06 12:41:32 -0400

    added to week 7

--- start.sql
+++ start.sql
@@ -1,8 +1,19 @@
 CREATE DATABASE users;
 use users;
+select database();
 
-create table emails(
-    -- fill out should have ID, name, and email
+create table scheduling_emails(
+user_Id int,
+user_name varchar (25),
+email varchar (255)
+
 );
+show tables;
 
--- maybe insert a test value
+insert into scheduling_emails(
+user_ID,
+user_name,
+email
+)
+values (1212, "joiuj ojh", "lkji@ylaj.com");
+select * from scheduling_emails;
